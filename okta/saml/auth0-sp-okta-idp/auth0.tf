0b0ee9fbb98444e84bf1ef5158b60f4415a559d7
Amin Abbaspour <amin.abbaspour@okta.com>
2026-08-06 13:38:10 +1000

    link with github and start adding SCIM

--- okta/saml/auth0-sp-okta-idp/auth0.tf
+++ okta/saml/auth0-sp-okta-idp/auth0.tf
@@ -1,23 +1,23 @@
 resource "auth0_connection" "okta-saml" {
-  name     = "Amin-Okta-SAML"
-  strategy = "samlp"
+  name           = "Amin-Okta-SAML"
+  strategy       = "samlp"
   display_name   = "Amin-Okta SAML Connection"
   show_as_button = true
 
   options {
-    debug               = false
-    sign_in_endpoint    = "https://amin.okta.com/app/amin_aminjp_1/exk3ns1r6dQbe5SHr3l7/sso/saml"
-    sign_out_endpoint   = "https://saml.provider/sign_out"
-    disable_sign_out    = true
-    tenant_domain       = "okta.com"
-    domain_aliases      = ["okta.com"]
-    protocol_binding    = "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"
-    user_id_attribute   = "https://saml.provider/imi/ns/identity-200810"
-    signature_algorithm = "rsa-sha256"
-    digest_algorithm    = "sha256"
-    icon_url            = "https://saml.provider/assets/logo.png"
-    metadata_xml = ""
-    metadata_url        = "https://amin.okta.com/app/exk3ns1r6dQbe5SHr3l7/sso/saml/metadata"
+    debug                    = false
+    sign_in_endpoint         = "https://amin.okta.com/app/amin_aminjp_1/exk3ns1r6dQbe5SHr3l7/sso/saml"
+    sign_out_endpoint        = "https://saml.provider/sign_out"
+    disable_sign_out         = true
+    tenant_domain            = "okta.com"
+    domain_aliases           = ["okta.com"]
+    protocol_binding         = "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"
+    user_id_attribute        = "https://saml.provider/imi/ns/identity-200810"
+    signature_algorithm      = "rsa-sha256"
+    digest_algorithm         = "sha256"
+    icon_url                 = "https://saml.provider/assets/logo.png"
+    metadata_url             = "https://amin.okta.com/app/exk3ns1r6dQbe5SHr3l7/sso/saml/metadata"
+    set_user_root_attributes = "on_each_login"
 
     idp_initiated {
       client_id              = data.auth0_client.JWT-io.client_id
@@ -26,12 +26,6 @@ resource "auth0_connection" "okta-saml" {
     }
 
 
-  }
-
-  lifecycle {
-    ignore_changes = [
-      set_user_root_attributes
-    ]
   }
 }
 
